deb46d6699bd02036b0b7460c2128c2d59b11ed8
AryPassos <93626414+AryPassos@users.noreply.github.com>
2021-11-12 17:42:57 -0300

    Update Torneiro de Tenis.cpp

--- Torneiro de Tenis.cpp
+++ Torneiro de Tenis.cpp
@@ -2,20 +2,20 @@
 #include <conio.h>
 
 int main(){
-	int jog = 0;
-	int vit = 0;
+	int jog = 0;//contador de partidas
+	int vit = 0;//contador de vitorias
 	char res;
 	while (jog < 6){
 		jog ++;
-		scanf(" %c", &res);
+		scanf(" %c", &res);//sistema lê a resultado da partida
 		if(res == 'V' || res == 'v'){
-			vit++;
+			vit++;//vitorias sendo contadas
 		}
 		else{
 
 		}
 	}
-	if(vit>=5)
+	if(vit>=5)//classsificação por grupo dependendo do numero de vitorias
 		printf("1");
 	else if(vit>=3)
 		printf("2");
